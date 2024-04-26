8f34d69bd8ff354fc169bc4040f4d1669ce56021
mia <mia_charlotte.henrichsmeyer@smail.th-koeln.de>
2024-04-26 12:14:21 +0200

    Searchbar fixed

---
+++ assets/scripts/main.js
@@ -0,0 +1,9 @@
+let burgermenu = document.querySelector('[data-js-main-menu-trigger]');
+
+// console.log(burgermenu);
+
+burgermenu.addEventListener('click', handleMenuClick);
+
+function handleMenuClick() {
+    consologe.log('The Main Menu was clicked')
+}
