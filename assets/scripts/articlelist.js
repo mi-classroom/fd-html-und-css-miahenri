53aeb810cbec19c68b424d00e45f379037ce0ce1
mia <mia_charlotte.henrichsmeyer@smail.th-koeln.de>
2024-05-03 12:12:22 +0200

    Dynamische Cards 1/3

---
+++ assets/scripts/articlelist.js
@@ -0,0 +1,25 @@
+const articleListURL = 'https://gist.githubusercontent.com/vschaefer/8d26be957bbc8607f60da5dd1b251a78/raw/49ddd7c2636fb722912d91107aff55c79ddf05a8/articles.json';
+
+document.addEventListener('DOMContentLoaded', function () {
+    createArticles();
+})
+
+function createArticles() {
+    const responsePromise = fetch(articleListURL); //fetch holt sich die Daten von der URL
+    responsePromise.then(function(response){ //then reagiert erst wenn Response ausgeführt wurde
+        const dataPromise = response.json(); //json() gibt ein Promise zurück
+        dataPromise.then(function(data){ //then reagiert erst wenn data ausgeführt wurde und löst Promise auf
+            renderArticleList(data.articles);
+        });
+    });
+}
+
+function renderArticleList(articles) {
+    articleListElement = document.querySelector('[data-js-generate-articleList]');
+    const authors = articles.map(function(article) {
+        console.log(article);
+        return `<li>${article.author}</li>`;
+    }).join('');
+    articleListElement.innerHTML = authors;
+    console.log(authors);
+}
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,46 @@
-let burgermenu = document.querySelector('[data-js-main-menu-trigger]');
+let burgermenu = document.querySelector("[data-js-main-menu-trigger]");
+let menu = document.querySelector("[data-js-main-menu]")
 
 // console.log(burgermenu);
 
-burgermenu.addEventListener('click', handleMenuClick);
+burgermenu.addEventListener("click", () => {
+    menu.classList.toggle("is-active");
+    burgermenu.classList.toggle("is-active");
+});
 
-function handleMenuClick() {
-    consologe.log('The Main Menu was clicked')
-}
+/* function handleMenuClick() {
+    menu.classList.toggle("is-active");
+    burgermenu.classList.toggle("is-active");
+    // console.log('The Main Menu was clicked');
+} */
+
+const tabLinks = document.querySelectorAll(".tab-link");
+const tabContents = document.querySelectorAll(".tab-content");
+const mainContent = document.querySelector("main");
+
+tabLinks.forEach((tabLink) => {
+  tabLink.addEventListener("click", function () {
+    const tabId = this.getAttribute("data-tab");
+
+    const isMeinBereich = this.textContent.trim() === "Mein Bereich";
+
+    if (!isMeinBereich) {
+      mainContent.classList.add("is-hidden");
+    } else {
+      mainContent.classList.remove("is-hidden");
+    }
+
+    tabLinks.forEach((link) => {
+      link.classList.remove("is-active");
+    });
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("is-active");
+    });
+
+    this.classList.add("is-active");
+
+    const selectedTabContent = document.getElementById(tabId);
+    selectedTabContent.classList.add("is-active");
+  });
+});
